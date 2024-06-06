7c999a26690a3033634d83c589a4819c251f9a34
D:/Cypress/Orange HRM <etumahmuda@gmail.com>
2024-06-06 12:05:13 +0600

    fix description

--- src/test/java/ViewProducts.java
+++ src/test/java/ViewProducts.java
@@ -7,13 +7,13 @@ import org.testng.annotations.Test;
 import javax.swing.*;
 
 public class ViewProducts extends BaseClass{
-@Test(priority = 0, description = "visit the website then scroll down for view items")
+@Test(priority = 0, description = "visit the website then scroll Up down for view items")
     public void viewItems() throws Exception{
         //scroll down
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
         Thread.sleep(1000);
-        //scroll down
+        //scroll up
         JavascriptExecutor js1 = (JavascriptExecutor) driver;
         js1.executeScript("window.scrollTo(document.body.scrollHeight, 0)");
         Thread.sleep(1000);
@@ -27,7 +27,7 @@ public class ViewProducts extends BaseClass{
 
 
     }
-    @Test(priority = 1, description = "hover on main menu")
+    @Test(priority = 1, description = "hover on multiple menu")
     public void hoverOnMenu() throws  Exception{
         //hover on main menu
         WebElement element_to_hover_main_menu = driver.findElement(By.xpath("//*[@id=\"menu-main-menu\"]/li[2]"));
